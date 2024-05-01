605e70b93e8f9d014c738053e56069275c7aebee
Jefferson Alves <166748180+JeffersonAlvesB@users.noreply.github.com>
2024-05-01 19:46:14 -0300

    Update script.js

--- formulário básico/assents/script.js
+++ formulário básico/assents/script.js
@@ -20,7 +20,7 @@ formulario.addEventListener("submit", (event) => {
   formulario.submit();
 });
 
-//Fução validar email
+//Função validar email
 function emailValido(emails) {
   const emailRegex = new RegExp(
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
